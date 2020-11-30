f56e114dcf42a485b73a73dd5cb4ce85889745bb
tom <271129083@qq.com>
2020-11-30 12:38:17 +0800

    网关异常重试

--- metaq-services/metaq-common-area-service/src/main/java/cn/metaq/common/area/web/AreaController.java
+++ metaq-services/metaq-common-area-service/src/main/java/cn/metaq/common/area/web/AreaController.java
@@ -5,8 +5,11 @@ import cn.metaq.common.area.domain.Area;
 import cn.metaq.common.area.dto.AreaDTO;
 import cn.metaq.common.core.dto.Pagination;
 import cn.metaq.common.web.BaseController;
+import cn.metaq.common.web.annotation.IgnoreResultWrapper;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.Resource;
+import java.io.IOException;
 import java.util.List;
 
 /**
@@ -35,4 +38,16 @@ public class AreaController extends BaseController<AreaBiz> {
         return baseBiz.list(areaDTO);
     }
 
+    @GetMapping("/retry")
+    @IgnoreResultWrapper
+    public String timeout() throws IOException {
+        try{
+            //睡5秒，网关Hystrix3秒超时，会触发熔断降级操作
+//            Thread.sleep(6000);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        throw new IOException("重试异常");
+//        return "retry";
+    }
 }
