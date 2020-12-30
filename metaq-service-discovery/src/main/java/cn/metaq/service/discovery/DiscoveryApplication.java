38942e0c3967303ed743ff53ea2201f7425fc366
tom <tangzan520@hotmail.com>
2020-12-30 18:37:15 +0800

    undertow替换tomcat

--- metaq-service-discovery/src/main/java/cn/metaq/service/discovery/DiscoveryApplication.java
+++ metaq-service-discovery/src/main/java/cn/metaq/service/discovery/DiscoveryApplication.java
@@ -1,14 +1,21 @@
 package cn.metaq.service.discovery;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.core.SpringVersion;
 
 @EnableEurekaServer
 @SpringBootApplication
 public class DiscoveryApplication {
 
 	public static void main(String[] args) {
+
+//		new SpringApplicationBuilder(DiscoveryApplication.class)
+//				.main(SpringVersion.class)
+//				.bannerMode(Banner.Mode.CONSOLE).run(args);
 		SpringApplication.run(DiscoveryApplication.class, args);
 	}
 
