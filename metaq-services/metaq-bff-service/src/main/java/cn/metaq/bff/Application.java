d51977d588524e8fb0a5bc645db4036adc91e669
tom <tangzan520@hotmail.com>
2021-01-20 16:07:28 +0800

    服务间调用权限

--- metaq-services/metaq-bff-service/src/main/java/cn/metaq/bff/Application.java
+++ metaq-services/metaq-bff-service/src/main/java/cn/metaq/bff/Application.java
@@ -2,8 +2,12 @@ package cn.metaq.bff;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"cn.metaq.bff","cn.metaq.common.web"})
+@EnableFeignClients
+@EnableOAuth2Sso
 public class Application {
 
     public static void main(String[] args) {
