f169e67241c92121ee7935e94581d3a5a7f545d9
tom <tangzan520@hotmail.com>
2021-02-08 10:53:56 +0800

    更新配置

--- metaq-service-discovery/src/main/java/cn/metaq/service/discovery/DiscoveryApplication.java
+++ metaq-service-discovery/src/main/java/cn/metaq/service/discovery/DiscoveryApplication.java
@@ -1,11 +1,8 @@
 package cn.metaq.service.discovery;
 
-import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.core.SpringVersion;
 
 @EnableEurekaServer
 @SpringBootApplication
